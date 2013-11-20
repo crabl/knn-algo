4a16f470c4c366fdbdb35d5cd9bec1c7574d9c5b
Chris Rabl <rabl.christopher@gmail.com>
2013-11-19 23:06:22 -0700

    Changed name to from_vectors from to_lattice_basis

--- src/lattice.py
+++ src/lattice.py
@@ -8,7 +8,7 @@ import numpy as np
 # vectors: set of vectors in R^n that we want to convert to V^n
 # vector: vector in R^n
 # basis_vectors: n linearly-independent vectors in R^n
-def to_lattice_basis(vectors, basis_vectors):
+def from_vectors(vectors, basis_vectors):
     A_inverse = np.linalg.inv(np.matrix(basis_vectors).transpose())
     new_vectors = []
     for vector in vectors:
