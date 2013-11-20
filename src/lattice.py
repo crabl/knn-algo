fc26b6c58cfcc57b8dd07274d6ae1334eac9ba19
Chris Rabl <rabl.christopher@gmail.com>
2013-11-19 23:04:45 -0700

    Now lattice basis changes actually work

--- src/lattice.py
+++ src/lattice.py
@@ -10,10 +10,10 @@ import numpy as np
 # basis_vectors: n linearly-independent vectors in R^n
 def to_lattice_basis(vectors, basis_vectors):
     A_inverse = np.linalg.inv(np.matrix(basis_vectors).transpose())
-    new_vectors = np.array([])
+    new_vectors = []
     for vector in vectors:
         v = A_inverse * np.matrix(vector).transpose()
         v = np.round(v)
         new_vectors.append(v)
 
-    return new_vectors
+    return np.array(new_vectors)
