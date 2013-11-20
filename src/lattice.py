a290522ec888baf032147ee67cb5fa6b7d638e3c
Chris Rabl <rabl.christopher@gmail.com>
2013-11-19 22:42:37 -0700

    Added vector basis change code

---
+++ src/lattice.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Snap n-dimensional points to an n-dimensional integer lattice
+# CPSC 4110 - Approximation Algorithms
+# Camara Lerner & Chris Rabl
+
+import numpy as np
+
+# vectors: set of vectors in R^n that we want to convert to V^n
+# vector: vector in R^n
+# basis_vectors: n linearly-independent vectors in R^n
+def to_lattice_basis(vectors, basis_vectors):
+    A_inverse = np.linalg.inv(np.matrix(basis_vectors).transpose())
+    new_vectors = np.array([])
+    for vector in vectors:
+        v = A_inverse * np.matrix(vector).transpose()
+        v = np.round(v)
+        new_vectors.append(v)
+
+    return new_vectors
