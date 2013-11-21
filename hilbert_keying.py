b9c5825055edd12c06cf51fa47c6d59b75c22307
Camara Lerner <camara.lerner@gmail.com>
2013-11-21 00:11:25 -0700

    added c and c++ versions of the stupid paper alg, and tried to fix it. STILL VERY VERY BROKEN

--- hilbert_keying.py
+++ hilbert_keying.py
@@ -4,37 +4,41 @@
 # DIM = n
 
 # calc_P
-# i is the ?????, and H is the point
+# i is a number, and H is the point
 def calc_P(i, H, n, ORDER):
+    MAX = 2 ** ORDER
     element = i / ORDER
     P = H[element]
     if(i % ORDER > ORDER - n):
         temp1 = H[element + 1]
-        P >>= i % ORDER
-        temp1 <<= ORDER - i % ORDER
+        P /= 2 ** (i % ORDER)
+        temp1 *= 2 ** (ORDER - i % ORDER)
+        P %= MAX
+        temp1 %= MAX
         P |= temp1
     else:
-        P >>= i % ORDER
+        P /= 2 ** (i % ORDER)
+        P %= MAX
     if n < ORDER:
-        P &= (1 << n) -1
+        P &= (2 ** n) - 1
     return P
 
 # calc_P2
 # returns p^i
 # S is sigma^i
-def calc_P2(S, n, g_mask):
+def calc_P2(S, n, g_mask, MAX):
     P = S & g_mask[0]
     for i in range(1, n):
-        if(S & g_mask[i] ** (P >> 1) & g_mask[i]):
+        if(S & ((g_mask[i] ** (P/2))% MAX) & g_mask[i]):
             P |= g_mask[i]
     return P
 
 # calc_J
-# P is ??????
+# P is a number, n is the dimensions
 def calc_J(P, n):
     J = n
     for i in range(1, n):
-        if(P >> i & 1) == (P & 1):
+        if(P/(2 ** i) & 1) == (P & 1):
             continue
         else:
             break
@@ -44,34 +48,38 @@ def calc_J(P, n):
 
 
 # calc_T
-# P is ??????
+# P is a number
 def calc_T(P):
     if P < 3:
         return 0
     if P % 2:
-        return (P - 1) ** (P - 1) / 2
-    return (P - 2) ** ( P - 2) / 2
+        return ((P - 1) ^ (P - 1)) / 2
+    return ((P - 2) ^ ( P - 2)) / 2
 
 # calc_tS_tT
 # xJ is ??????? and val is ????????
-def calc_tS_tT(xJ, val, n):
+def calc_tS_tT(xJ, val, n, MAX):
     retval = val
     if(xJ % n != 0):
-        temp1 = val >> xJ % n
-        temp2 = val << n - xJ % n
+        temp1 = val / (2 ** xJ % n)
+        temp2 = (val * (2 ** n - xJ % n)) % MAX
         retval = temp1 | temp2
-        retval &= (1 << n) - 1
+        retval &= (2 ** n) - 1
     return retval
 
 
 # hilbert_key_encode
 # point is an n dimensional pt, n is the dimensions, 
 # and m is the order of the curve
-def hilbert_key_encode(point, n, m):
+def hilbert_key_encode(n, m, point):
+    pt = []
+    for x in point:
+        pt.append(x)
     ORDER = 32
+    MAX = 2 ** ORDER
     g_mask = []
     for j in range(0, n):
-        g_mask.append(1 << j)
+        g_mask.append(2 ** j)
     g_mask.reverse()
     mask = 2 ** ORDER - 1
     W = 0
@@ -80,46 +88,48 @@ def hilbert_key_encode(point, n, m):
     i = ORDER * n - n
     A = 0
     for j in range(0, n):
-        if(point[j] & mask):
+        if(pt[j] & mask):
             A |= g_mask[j]
+    print A
     S = tS = A
-    P = calc_P2(S, n, g_mask)
+    P = calc_P2(S, n, g_mask, MAX)
     element = i / ORDER
     if(i % ORDER > ORDER - n):
-        h[element] |= P << i % ORDER
-        h[element + 1] |= P >> ORDER - i % ORDER
+        h[element] |= (P * ( 2 ** i % ORDER)) % MAX
+        h[element + 1] |= P / ( 2 ** ORDER - i % ORDER)
     else:
-        h[element] |= P << i - element * ORDER
+        h[element] |= (P * (2 ** i - element * ORDER)) % MAX
     
     J = calc_J(P, n)
     xJ = J - 1
     T = calc_T(P)
     tT = T
 
-    mask >>= 1
+    mask /= 2
     i -= n
     while i >= 0:
-        j = A = 0
+        A = 0
         for j in range(0, n):
-            if point[j] & mask:
+            if pt[j] & mask:
                 A |= g_mask[j]
-        W = W ** tT
-        tS = A ** W
-        S = calc_tS_tT(xJ, tS, n)
-        P = calc_P2(S, n, g_mask)
+        W ^= tT
+        tS = A ^ W
+        S = calc_tS_tT(xJ, tS, n, MAX)
+        P = calc_P2(S, n, g_mask, MAX)
         element = i / ORDER
         if i % ORDER > ORDER - n:
-            h[element] |= P << i % ORDER
-            h[element + 1] |= P >> ORDER - i % ORDER
+            h[element] |= (P * (2 ** i % ORDER)) % MAX
+            h[element + 1] |= P / (2 ** ORDER - i % ORDER)
         else:
-            h[element] |= P << i - element * ORDER
+            h[element] |= (P * (2 ** i - element * ORDER)) % MAX
         if i > 0:
             T = calc_T(P)
-            tT = calc_tS_tT(xJ, T, n)
+            tT = calc_tS_tT(xJ, T, n, MAX)
             J = calc_J(P, n)
             xJ += J - 1
+        print W    
         i -= n
-        mask >>= 1
+        mask /= 2
 
     return h
         
@@ -130,7 +140,7 @@ def hilbert_key_encode(point, n, m):
 # and m is the order of the curve
 """
 def hilbert_key_decode(point, n, m):
-    mask = 1 << ORDER - 1
+    mask = 2 ** ORDER - 1
     W = 0
     P = 0
     pt = [0]*n
