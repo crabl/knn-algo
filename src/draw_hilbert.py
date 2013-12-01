a068c682334c4d58d6ebb8deda1dc65aaa1007d3
Chris Rabl <rabl.christopher@gmail.com>
2013-12-01 15:03:47 -0700

    Renamed drawing program

--- src/draw_hilbert.py
+++ src/draw_hilbert.py
@@ -7,16 +7,16 @@ import random
 def draw_graph_2d(points, path, file_name):
     plt.clf()
     fig = plt.figure()
-    ax = fig.add_axes([0, 0, 1, 1])
-    ax.axis('off')
 
     x = [v[0] for v in path]
     y = [v[1] for v in path]
-    ax.plot(x,y, color='#000000', ls='-', alpha=0.25)
+    plt.plot(x,y, color='#FF0000', ls='-', alpha=0.45)
 
     point_x = [p[0] for p in points]
     point_y = [p[1] for p in points]
-    ax.plot(point_x, point_y, marker='o', color='#BEF202', ls='')
+    plt.plot(point_x, point_y, marker='o', color='#BEF202', ls='')
+
+    plt.axis([-1,16, -1, 16])
 
     fig.savefig(file_name, filetype="jpg")
 
@@ -41,25 +41,24 @@ def draw_graph_3d(points, path, file_name, el, az):
     fig.savefig(file_name, filetype="jpg")
 
 def random_2d_point():
-    return (random.randint(0,7), random.randint(0,7))
+    return (random.randint(0,15), random.randint(0,15))
 
 def random_3d_point():
-    return (random.randint(0,7), random.randint(0,7), random.randint(0,7))
+    return (random.randint(0,15), random.randint(0,15), random.randint(0,15))
 
 def main():
-    points_8 = [(x,y,z) for x in range(8) for y in range(8) for z in range(8)]
-    #points_8 = [(x,y) for y in range(8) for x in range(8)]
+    #points_8 = [(x,y,z) for x in range(8) for y in range(8) for z in range(8)]
+    points_8 = [(x,y) for y in range(16) for x in range(16)]
     print "Performing Hilbert ordering..."
-    hilbert_path = [hilbertIndex(3,8,point) for point in points_8]
+    hilbert_path = [hilbertIndex(2,16,point) for point in points_8]
     ordered_path = [point for (hilbert_index,point) in sorted(zip(hilbert_path, points_8))]
-    data_set = [random_3d_point() for i in range(20)]
-    #data_set = [random_2d_point() for i in range(20)]
-    print ordered_path
+    #data_set = [random_3d_point() for i in range(20)]
+    data_set = [random_2d_point() for i in range(20)]
     print "Drawing graph..."
 
     for i in range(len(ordered_path)):
-        draw_graph_3d(data_set, ordered_path[0:i], "animation/hilbert"+str(i)+".jpg", 30, i % 360)
-        #draw_graph_2d(data_set, ordered_path[0:i], "animation/hilbert"+str(i)+".jpg")
+        #draw_graph_3d(data_set, ordered_path[0:i], "animation/hilbert"+str(i)+".jpg", 30, i % 360)
+        draw_graph_2d(data_set, ordered_path[0:i], "animation/hilbert"+str(i)+".jpg")
     
 if __name__ == "__main__":
     main()
