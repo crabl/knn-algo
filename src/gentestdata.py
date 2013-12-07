c90f0e9d7e78f57849ef17ff0d501d7076841282
Chris Rabl <rabl.christopher@gmail.com>
2013-12-06 23:22:54 -0700

    Generating random test data now

--- src/gentestdata.py
+++ src/gentestdata.py
@@ -1,8 +1,10 @@
-f = open("lattice_40x40.csv", "w")
+import random
 
-points = [(x,y) for x in range(40) for y in range(40)]
+f = open("lattice_40x40_random.csv", "w")
 
-for (x,y) in points:
-    f.write(str(x)+"\t"+str(y)+"\n")
+points = [(random.randint(0,100),random.randint(0,100),random.randint(0,100)) for x in range(40) for y in range(40) for  z in range(40)]
+
+for (x,y,z) in points:
+    f.write(str(x)+"\t"+str(y)+"\t"+str(z)+"\n")
 
 f.close()
